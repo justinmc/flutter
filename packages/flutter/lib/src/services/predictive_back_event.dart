3c02896c6425892f5a47d73f382c537af495a28b
Justin McCandless <jmccandless@google.com>
2024-03-25 10:17:11 -0700

    Docs and code cleanup from Renzo's review

--- packages/flutter/lib/src/services/predictive_back_event.dart
+++ packages/flutter/lib/src/services/predictive_back_event.dart
@@ -18,8 +18,8 @@ enum SwipeEdge {
 
 /// Object used to report back gesture progress in Android.
 ///
-/// Holds information about the touch event, swipe direction and the animation
-/// progress that predictive back animations should seek to.
+/// Holds information about the touch event, swipe direction, and the animation
+/// progress that predictive back animations should follow.
 @immutable
 class PredictiveBackEvent {
   /// Creates a new [PredictiveBackEvent] instance.
@@ -49,25 +49,25 @@ class PredictiveBackEvent {
   /// button press.
   final double? y;
 
-  /// Returns a value between 0 and 1 representing how far along the back
+  /// Returns a value between 0.0 and 1.0 representing how far along the back
   /// gesture is.
   ///
   /// This value is driven by the horizontal location of the touch point, and
   /// should be used as the fraction to seek the predictive back animation with.
   /// Specifically,
   ///
-  /// - The progress is 0 when the touch is at the starting edge of the screen
+  /// - The progress is 0.0 when the touch is at the starting edge of the screen
   ///   (left or right), and the animation should seek to its start state.
-  /// - The progress is approximately 1 when the touch is at the opposite side
+  /// - The progress is approximately 1.0 when the touch is at the opposite side
   ///   of the screen, and the animation should seek to its end state. Exact end
   ///   value may vary depending on screen size.
   ///
   /// When the gesture is canceled, the progress value continues to update,
-  /// animating back to 0 until the cancellation animation completes.
+  /// animating back to 0.0 until the cancellation animation completes.
   ///
   /// In-between locations are linearly interpolated based on horizontal
-  /// distance from the starting edge and smooth clamped to 1 when the distance
-  /// exceeds a system-wide threshold.
+  /// distance from the starting edge and smooth clamped to 1.0 when the
+  /// distance exceeds a system-wide threshold.
   final double progress;
 
   /// The screen edge from which the swipe gesture starts.
@@ -79,7 +79,7 @@ class PredictiveBackEvent {
   /// opts for 3-button navigation. In cases of gesture navigation, it returns
   /// false.
   bool get isButtonEvent =>
-      x == null || y == null || (progress == 0 && x == 0 && y == 0);
+      x == null || y == null || (progress == 0.0 && x == 0.0 && y == 0.0);
 
   @override
   bool operator ==(Object other) {
